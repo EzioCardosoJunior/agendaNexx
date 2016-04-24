43b0b9d9a5ad5bf338c5f8c162706b40a212aa77
EzioCardosoJunior <ejcj1980@msn.com>
2016-04-24 14:50:28 -0300

    1

---
+++ angularMaterial/html/scripts/ui-jp.config.js
@@ -0,0 +1,24 @@
+// lazyload config
+
+var jp_config = {
+  easyPieChart:   [ 'bower_components/jquery.easy-pie-chart/dist/jquery.easypiechart.fill.js' ],
+  sparkline:      [ 'bower_components/jquery.sparkline/dist/jquery.sparkline.retina.js' ],
+  plot:           [ 'bower_components/flot/jquery.flot.js',
+                    'bower_components/flot/jquery.flot.resize.js',
+                    'bower_components/flot/jquery.flot.pie.js',
+                    'bower_components/flot.tooltip/js/jquery.flot.tooltip.min.js',
+                    'bower_components/flot-spline/js/jquery.flot.spline.min.js',
+                    'bower_components/flot.orderbars/js/jquery.flot.orderBars.js'],
+  vectorMap:      [ 'bower_components/bower-jvectormap/jquery-jvectormap-1.2.2.min.js',
+                    'bower_components/bower-jvectormap/jquery-jvectormap.css', 
+                    'bower_components/bower-jvectormap/jquery-jvectormap-world-mill-en.js',
+                    'bower_components/bower-jvectormap/jquery-jvectormap-us-aea-en.js' ],
+  dataTable:      [
+                    'bower_components/datatables/media/js/jquery.dataTables.min.js',
+                    'bower_components/plugins/integration/bootstrap/3/dataTables.bootstrap.js',
+                    'bower_components/plugins/integration/bootstrap/3/dataTables.bootstrap.css'],
+  footable:       [
+                    'bower_components/footable/dist/footable.all.min.js',
+                    'bower_components/footable/css/footable.core.css'
+                  ]
+};
